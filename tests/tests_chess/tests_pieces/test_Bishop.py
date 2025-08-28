51beda485eda92e41d1652183741f416233797a1
DeanElizardo <del@delizardo.com>
2025-08-28 07:07:58 -0400

    started bishop tests

---
+++ tests/tests_chess/tests_pieces/test_Bishop.py
@@ -0,0 +1,58 @@
+import unittest
+from src.chess.pieces.Bishop import Bishop
+from src.chess.Kernel import *
+from src.chess.Space import Space
+
+class TestBishop(unittest.TestCase):
+    def bishop_factory(self, color: str, space: Space) -> Bishop:
+        return Bishop(color, space)
+    
+    def make_white_bishop(self, space: Space) -> Bishop:
+        return self.bishop_factory(WHITE, space)
+    
+    def make_black_bishop(self, space: Space) -> Bishop:
+        return self.bishop_factory(BLACK, space)
+    
+    def generate_expected_spaces(self, init_space: Space) -> list[Space]:
+        expected_spaces: list[Space] = []
+        s = Space(init_space.file, init_space.rank)
+        while s.increase_file() is not None and s.increase_rank() is not None:
+            s = Space(s.increase_file(), s.increase_rank())
+            expected_spaces.append(s)
+        s = Space(init_space.file, init_space.rank)
+        while s.increase_file() is not None and s.decrease_rank() is not None:
+            s = Space(s.increase_file(), s.decrease_rank())
+            expected_spaces.append(s)
+        s = Space(init_space.file, init_space.rank)
+        while s.decrease_file() is not None and s.increase_rank() is not None:
+            s = Space(s.decrease_file(), s.increase_rank())
+            expected_spaces.append(s)
+        s = Space(init_space.file, init_space.rank)
+        while s.decrease_file() is not None and s.decrease_rank() is not None:
+            s = Space(s.decrease_file(), s.decrease_rank())
+            expected_spaces.append(s)
+        expected_spaces = sorted(expected_spaces, key=lambda p: (p.file, p.rank))
+        return expected_spaces
+    
+    def white_bishop_has_right_moves_on_init(self, init_space: Space, expected_spaces: list[Space]):
+        bishop = self.make_white_bishop(init_space)
+        actual_spaces: list[Space] = []
+        for m in bishop.moves:
+            for s in m:
+                actual_spaces.append(s)
+        actual_spaces= sorted(actual_spaces, key=lambda p: (p.file, p.rank))
+
+        assert len(actual_spaces) == len(expected_spaces)
+
+        for idx in range(len(expected_spaces)):
+            assert actual_spaces[idx] == expected_spaces[idx]
+
+    def test_white_init_c1(self):
+        init_space = Space('c', 1)
+        expected_spaces: list[Space] = self.generate_expected_spaces(init_space)
+        self.white_bishop_has_right_moves_on_init(init_space, expected_spaces)
+
+    def test_white_init_f1(self):
+        init_space = Space('f', 1)
+        expected_spaces: list[Space] = self.generate_expected_spaces(init_space)
+        self.white_bishop_has_right_moves_on_init(init_space, expected_spaces)
