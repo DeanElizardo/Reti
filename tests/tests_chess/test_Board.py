69e90a0016064e68b92b6655183beb8717e809d4
DeanElizardo <del@delizardo.com>
2025-09-09 16:02:09 -0400

    additional tests

--- tests/tests_chess/test_Board.py
+++ tests/tests_chess/test_Board.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import unittest
 from dataclasses import dataclass
-# from typing import Union, Dict
+from typing import Dict
 from src.chess.Kernel import *
 from src.chess.Board import Board
 from src.chess.Space import Space
@@ -63,6 +63,41 @@ class TestBoard(unittest.TestCase):
         openings = sorted(openings, key=lambda p: (p[0].file, p[0].rank))
         return openings
 
+    def generate_pawn_opened_expected(self, pawn: Pawn, opened_space: Space) -> list[list[Space]]:
+        expected: list[list[Space]] = []
+        lesser_file = pawn.position.decrease_file()
+        greater_file = pawn.position.increase_file()
+        if pawn.color == WHITE:
+            expected.append([
+                Space(
+                    opened_space.file,
+                    opened_space.increase_rank()
+                )
+            ])
+        else:
+            expected.append([
+                Space(
+                    opened_space.file,
+                    opened_space.decrease_rank()
+                )
+            ])
+        if lesser_file is not None:
+            expected.append([
+                Space(
+                    lesser_file,
+                    expected[0][0].rank
+                )
+            ])
+        if greater_file is not None:
+            expected.append([
+                Space(
+                    greater_file,
+                    expected[0][0].rank
+                )
+            ])
+        expected = sorted(expected, key=lambda p: (p[0].file, p[0].rank))
+        return expected
+
     def confirm_moves(self, case: CaseData):
         board = case.input.board
         piece = case.input.piece
@@ -87,7 +122,7 @@ class TestBoard(unittest.TestCase):
                 for s in expected_moves:
                     expect_str += f"{s} "
                 test_case: CaseData = CaseData(
-                    InputData(board, piece),
+                    InputData(board, piece, piece.position),
                     expected_moves
                     )
                 with self.subTest(f"expect {expected_moves[:-1]}", piece=piece):
@@ -104,15 +139,123 @@ class TestBoard(unittest.TestCase):
                 for s in expected_moves:
                     expect_str += f"{s} "
                 test_case: CaseData = CaseData(
-                    InputData(board, piece), 
+                    InputData(board, piece, piece.position), 
                     expected_moves
                     )
                 with self.subTest(f"expect {expected_moves[:-1]}", piece=piece):
                     self.confirm_moves(test_case)
 
     def test_white_pawn_opening(self):
-        # TODO: Write tests that deal with pawn openings for black and white
-        pass
+        board = Board()
+        available_moves: Dict[Piece, list[str]] = {}
+        all_moves = board.offer_moves(WHITE)
+        for k in all_moves:
+            moves = all_moves[k]
+            if len(moves) > 0 and "Pinned" not in moves:
+                available_moves[k] = moves
+
+        for piece in available_moves:
+            if isinstance(piece, Pawn):
+                for move in available_moves[piece]:
+                    [file, rank] = list(move)
+                    move_space = Space(file, int(rank))
+                    expected: list[list[Space]] = self.generate_pawn_opened_expected(piece, move_space)
+                    test_case: CaseData = CaseData(
+                        InputData(board, piece, move_space),
+                        expected
+                    )
+                    with self.subTest(f"{expected}", move=f"{piece} -> {move_space}"):
+                        board.apply_move(piece, move_space)
+                        self.confirm_moves(test_case)
+                        assert piece.developed
+    
+    def test_black_pawn_opening(self):
+        board = Board()
+        available_moves: Dict[Piece, list[str]] = {}
+        all_moves = board.offer_moves(BLACK)
+        for k in all_moves:
+            moves = all_moves[k]
+            if len(moves) > 0 and "Pinned" not in moves:
+                available_moves[k] = moves
+        
+        for piece in available_moves:
+           if isinstance(piece, Pawn):
+                for move in available_moves[piece]:
+                    [file, rank] = list(move)
+                    move_space = Space(file, int(rank))
+                    expected: list[list[Space]] = self.generate_pawn_opened_expected(piece, move_space)
+                    test_case: CaseData = CaseData(
+                        InputData(board, piece, move_space),
+                        expected
+                    )
+                    with self.subTest(f"{expected}", move=f"{piece} -> {move_space}"):
+                        board.apply_move(piece, move_space)
+                        self.confirm_moves(test_case)
+                        assert piece.developed
+    
+    def generate_knight_opened_expected(self, knight: Knight, move: Space) -> list[list[Space]]:
+        expected: list[list[Space]] = []
+        new_rank = None
+        if knight.color == WHITE:
+            new_rank = knight.position.increase_rank()
+        else:
+            new_rank = knight.position.decrease_rank()
+        lesser_file = knight.position.decrease_file()
+        greater_file = knight.position.increase_file()
+        expected.extend([
+            [Space(lesser_file, new_rank)],
+            [Space(greater_file, new_rank)]
+        ])
+        expected = sorted(expected, key=lambda p: (p[0].file, p[0].rank))
+        return expected
+
+    def test_white_knight_opening(self):
+        board = Board()
+        available_moves: Dict[Piece, list[str]] = {}
+        all_moves = board.offer_moves(WHITE)
+        for k in all_moves:
+            moves = all_moves[k]
+            if len(moves) > 0 and "Pinned" not in moves:
+                available_moves[k] = moves
+        
+        for piece in available_moves:
+           if isinstance(piece, Knight):
+                for move in available_moves[piece]:
+                    [file, rank] = list(move[1:])
+                    move_space = Space(file, int(rank))
+                    expected: list[list[Space]] = self.generate_knight_opened_expected(piece, move_space)
+                    test_case: CaseData = CaseData(
+                        InputData(board, piece, move_space),
+                        expected
+                    )
+                    with self.subTest(f"{expected}", move=f"{piece} -> {move_space}"):
+                        board.apply_move(piece, move_space)
+                        self.confirm_moves(test_case)
+                        assert piece.developed
+
+    def test_black_knight_opening(self):
+        board = Board()
+        available_moves: Dict[Piece, list[str]] = {}
+        all_moves = board.offer_moves(BLACK)
+        for k in all_moves:
+            moves = all_moves[k]
+            if len(moves) > 0 and "Pinned" not in moves:
+                available_moves[k] = moves
+        
+        for piece in available_moves:
+           if isinstance(piece, Knight):
+                for move in available_moves[piece]:
+                    [file, rank] = list(move)
+                    move_space = Space(file, int(rank))
+                    expected: list[list[Space]] = self.generate_knight_opened_expected(piece, move_space)
+                    test_case: CaseData = CaseData(
+                        InputData(board, piece, move_space),
+                        expected
+                    )
+                    with self.subTest(f"{expected}", move=f"{piece} -> {move_space}"):
+                        board.apply_move(piece, move_space)
+                        self.confirm_moves(test_case)
+                        assert piece.developed
 
 @dataclass
 class CaseData:
@@ -123,3 +266,4 @@ class CaseData:
 class InputData:
     board: Board
     piece: Piece
+    move: Space
